5b2411f832439f66c7f243cb925060a5ace90c0a
varunnair82 <varun.nair82@gmail.com>
2022-06-07 23:44:26 +0530

    Initial WinFrmChangeColorWithVoice
    Initial WinFrmChangeColorWithVoice commit

---
+++ WinFrmChangeColorWithVoice/WinFrmChangeColorWithVoice/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSpeechRecognized = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnRecognise = new System.Windows.Forms.Button();
+            this.rdbMic = new System.Windows.Forms.RadioButton();
+            this.grp_VoiceInputMethod = new System.Windows.Forms.GroupBox();
+            this.rdbAudioFile = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.grp_VoiceInputMethod.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblSpeechRecognized
+            // 
+            this.lblSpeechRecognized.AutoSize = true;
+            this.lblSpeechRecognized.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSpeechRecognized.Location = new System.Drawing.Point(12, 69);
+            this.lblSpeechRecognized.Name = "lblSpeechRecognized";
+            this.lblSpeechRecognized.Size = new System.Drawing.Size(279, 31);
+            this.lblSpeechRecognized.TabIndex = 0;
+            this.lblSpeechRecognized.Text = "Speech Recognized : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(279, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = " ";
+            // 
+            // btnRecognise
+            // 
+            this.btnRecognise.Location = new System.Drawing.Point(388, 13);
+            this.btnRecognise.Name = "btnRecognise";
+            this.btnRecognise.Size = new System.Drawing.Size(75, 23);
+            this.btnRecognise.TabIndex = 2;
+            this.btnRecognise.Text = "Recognise";
+            this.btnRecognise.UseVisualStyleBackColor = true;
+            this.btnRecognise.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rdbMic
+            // 
+            this.rdbMic.AutoSize = true;
+            this.rdbMic.Location = new System.Drawing.Point(126, 19);
+            this.rdbMic.Name = "rdbMic";
+            this.rdbMic.Size = new System.Drawing.Size(42, 17);
+            this.rdbMic.TabIndex = 3;
+            this.rdbMic.TabStop = true;
+            this.rdbMic.Text = "Mic";
+            this.rdbMic.UseVisualStyleBackColor = true;
+            this.rdbMic.CheckedChanged += new System.EventHandler(this.rdbMic_CheckedChanged);
+            // 
+            // grp_VoiceInputMethod
+            // 
+            this.grp_VoiceInputMethod.Controls.Add(this.rdbAudioFile);
+            this.grp_VoiceInputMethod.Controls.Add(this.rdbMic);
+            this.grp_VoiceInputMethod.Location = new System.Drawing.Point(18, 12);
+            this.grp_VoiceInputMethod.Name = "grp_VoiceInputMethod";
+            this.grp_VoiceInputMethod.Size = new System.Drawing.Size(186, 49);
+            this.grp_VoiceInputMethod.TabIndex = 4;
+            this.grp_VoiceInputMethod.TabStop = false;
+            this.grp_VoiceInputMethod.Text = "Voice Input Method";
+            // 
+            // rdbAudioFile
+            // 
+            this.rdbAudioFile.AutoSize = true;
+            this.rdbAudioFile.Checked = true;
+            this.rdbAudioFile.Location = new System.Drawing.Point(15, 19);
+            this.rdbAudioFile.Name = "rdbAudioFile";
+            this.rdbAudioFile.Size = new System.Drawing.Size(68, 17);
+            this.rdbAudioFile.TabIndex = 4;
+            this.rdbAudioFile.TabStop = true;
+            this.rdbAudioFile.Text = "Audio file";
+            this.rdbAudioFile.UseVisualStyleBackColor = true;
+            this.rdbAudioFile.CheckedChanged += new System.EventHandler(this.rdbAudioFile_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(239, 168);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(224, 31);
+            this.textBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 213);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.grp_VoiceInputMethod);
+            this.Controls.Add(this.btnRecognise);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblSpeechRecognized);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grp_VoiceInputMethod.ResumeLayout(false);
+            this.grp_VoiceInputMethod.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSpeechRecognized;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnRecognise;
+        private System.Windows.Forms.RadioButton rdbMic;
+        private System.Windows.Forms.GroupBox grp_VoiceInputMethod;
+        private System.Windows.Forms.RadioButton rdbAudioFile;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WinFrmChangeColorWithVoice/WinFrmChangeColorWithVoice/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private SpeechRecognitionEngine sr = null;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Create a new SpeechRecognitionEngine instance.
+            //SpeechRecognizer sr = new SpeechRecognizer();
+            sr = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("en-US"));
+
+
+            //sr.SetInputToDefaultAudioDevice();
+            sr.SetInputToWaveFile(@"..\..\sound\Colors.wav");
+
+            // Create a simple grammar that recognizes "red", "green", or "blue".
+            Choices colors = new Choices();
+            colors.Add(new string[] { "yellow", "blue", "green", "orange", "white", "black", "purple", "red", "Lime", "tomato", "RoyalBlue"});
+
+            // Create a GrammarBuilder object and append the Choices object.
+            GrammarBuilder gb = new GrammarBuilder();
+            gb.Append(colors);
+
+            Grammar g = new Grammar(gb);
+            sr.LoadGrammarAsync(g);
+            
+            sr.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(sr_SpeechRecognized);
+            sr.RecognizeCompleted += new EventHandler<RecognizeCompletedEventArgs>(recognizer_RecognizeCompleted);
+
+            sr.SpeechDetected += Sr_SpeechDetected;
+
+            //sr.RecognizeAsync();
+        }
+
+        private void Sr_SpeechDetected(object sender, SpeechDetectedEventArgs e)
+        {
+            //MessageBox.Show("Speech detected " + e.AudioPosition.Ticks.ToString());
+        }
+
+        // Create a simple handler for the SpeechRecognized event.
+        private void sr_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            label2.Text = e.Result.Text;
+            if (e.Result.Confidence >= 0.7)
+            {
+                //high enough confidence, use result
+                //MessageBox.Show(e.Result.Text);
+                switch (e.Result.Text.ToLower())
+                {
+                    case "red":
+                        this.BackColor = Color.Red;
+                        break;
+                    case "blue":
+                        this.BackColor = Color.Blue;
+                        break;
+                    case "green":
+                        this.BackColor = Color.Green;
+                        break;
+                    case "orange":
+                        this.BackColor = Color.Orange;
+                        break;
+                    case "white":
+                        this.BackColor = Color.White;
+                        break;
+                    case "black":
+                        this.BackColor = Color.Black;
+                        break;
+                    case "purple":
+                        this.BackColor = Color.Purple;
+                        break;
+                    case "yellow":
+                        this.BackColor = Color.Yellow;
+                        break;
+                    case "lime":
+                        this.BackColor = Color.Lime;
+                        break;
+                    case "tomato":
+                        this.BackColor = Color.Tomato;
+                        break;
+                    case "royalblue":
+                        this.BackColor = Color.RoyalBlue;
+                        break;
+
+                }
+            }
+            else
+            {
+                //reject result
+            }           
+        }
+
+        bool completed = true;
+        // Handle the RecognizeCompleted event.
+        void recognizer_RecognizeCompleted(object sender, RecognizeCompletedEventArgs e)
+            {
+                if (e.Error != null)
+                {
+                    MessageBox.Show(string.Format("  Error encountered, {0}: {1}",e.Error.GetType().Name, e.Error.Message));
+                }
+                if (e.Cancelled)
+                {
+                    MessageBox.Show("  Operation cancelled.");
+                }
+                if (e.InputStreamEnded)
+                {
+                    MessageBox.Show("  End of stream encountered.");
+                }
+            }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (completed)
+                {
+                    if (rdbAudioFile.Checked && sr != null)
+                    {
+                        sr.RecognizeAsync(RecognizeMode.Single);
+                    }
+                    else
+                    {
+                        sr.RecognizeAsync(RecognizeMode.Multiple);
+                    }
+                }
+                else
+                {
+                    completed = false;
+                }
+            }catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void rdbAudioFile_CheckedChanged(object sender, EventArgs e)
+        {
+            if(rdbAudioFile.Checked && sr != null)
+            {
+                sr.SetInputToWaveFile(@"c:\sound\Colors.wav");
+            }
+        }
+
+        private void rdbMic_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdbMic.Checked && sr != null)
+            {
+                sr.SetInputToDefaultAudioDevice();
+            }
+        }
+    }
+}
